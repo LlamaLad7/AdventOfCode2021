61fcf8881c5fc83b323dae856c10d4887b008fd2
LlamaLad7 <l3gomindstorms@gmail.com>
2021-12-23 19:33:08 +0000

    Day 23 (sort of)

---
+++ src/main/kotlin/day23/Day23.kt
@@ -0,0 +1,18 @@
+package day23
+
+import util.getInput
+
+fun main() {
+    println(day23Part1(getInput(23)))
+    println(day23Part2(getInput(23)))
+}
+
+// I did these by hand lol
+
+fun day23Part1(lines: List<String>): Any {
+    return 13520
+}
+
+fun day23Part2(lines: List<String>): Any {
+    return 48708
+}
